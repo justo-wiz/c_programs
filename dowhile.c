27be9544e7dc41212e48ba854766b943a40a8115
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-07 10:58:13 +0300

    simple dowhile

---
+++ dowhile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * int main - Entry point
+ *
+ * Return: 0 Always.
+ */
+
+int main()
+{
+	int i;
+	i = 0;
+	
+	do 
+	{
+	printf("%d\n", i);
+	i--;
+	}
+	while (i > 0);
+	return (0);
+}
