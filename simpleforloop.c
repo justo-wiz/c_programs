3fd94f4d432ca55631e9aa7f548b82562b3de2a8
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-07 10:23:21 +0300

    for loop

---
+++ simpleforloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * int main - Entry point
+ *
+ * Return: 0 Always Success
+ */
+
+int main()
+{
+	int i = 3;
+	for (i = 3; i > 0; i--)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
