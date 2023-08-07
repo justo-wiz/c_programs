1ad7c53d122040c899b15b3e25ab7c31509a4189
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-07 10:21:48 +0300

    while loop

---
+++ simplewhileloop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * int main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main()
+{
+	int i = 3;
+	
+	while (i > 0)
+	{
+	printf("%d\n", i);
+	i--;
+	}
+	return (0);
+}
+
