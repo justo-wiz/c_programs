9d1141ae62c3790571ad34d7e6c6a4ca3ffcaebc
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-07 11:11:43 +0300

    more do while

---
+++ more01_dowhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * int main - Entry point 
+ *
+ * Return: Always 0
+ */
+
+int main()
+{
+	int n;
+
+	do
+	{
+	printf("Enter an Integer\n");
+	scanf("%d", &n);
+
+	}while (n != 0);
+}
